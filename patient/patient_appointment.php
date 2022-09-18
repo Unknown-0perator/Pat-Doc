fc3d2bbe297e28cd0f092eddb0ffb444e6d58a26
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-09-17 19:48:27 -0700

    Navbar added to appointment feature

--- patient/patient_appointment.php
+++ patient/patient_appointment.php
@@ -26,6 +26,25 @@ mysqli_query($con,"update appointment set userStatus='0' where id ='".$_GET['id'
     <title>Appointment</title>
 </head>
 <body>
+<div>
+        <nav class="navbar navbar-light bg-light top-nav">
+            <span class="navbar-brand h1 ">PatDoc</span>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="./patient_dashboard.php">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="./patient_reward.php">Rewards</a>
+                </li>
+                <li class="nav-item">
+                    <div class="dropdown">
+                    <a class="btn btn-secondary" href="./patient_sign_out.php">Logout</a>
+                    </div>
+                </li>
+            </ul>
+
+        </nav>
+    </div>
 <section id="page-title">
         <div style="padding: 2%;">
             <h2>Patient | Appointment History</h2>
