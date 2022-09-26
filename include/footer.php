8a9cf6e7a93441884d8536cb724f41bcb0c01cb7
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-09-25 19:07:41 -0700

    CSS code refactoring

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,4 @@
-<div style="text-align: center;padding-top: 2%; padding-bottom: 1%;">
+<div class = "footer"style="text-align: center;padding-top: 2%; padding-bottom: 1%;">
         <span>© PatDoc
             <script>document.write(new Date().getFullYear())</script>. All rights reserved
         </span>
