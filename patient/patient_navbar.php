06278ba7da2d0decaa9e713169015c4e3c331082
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2022-09-27 19:51:59 -0700

    Navbar bugs fixed

--- patient/patient_navbar.php
+++ patient/patient_navbar.php
@@ -1,19 +1,23 @@
 <div>
-        <nav class="navbar navbar-light bg-light top-nav">
-            <span class="navbar-brand h1 ">PatDoc</span>
-            <ul class="nav">
-                <li class="nav-item">
+        
+        <nav class="navbar navbar-dark bg-dark fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Pat-Doc</a>
+                <ul class="nav">
+                    <li class="nav-item">
                     <a class="nav-link" href="./patient_dashboard.php">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="./patient_reward.php">Rewards</a>
-                </li>
-                <li class="nav-item">
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="./patient_reward.php">Rewards</a>
+                    </li>
+                    <li class="nav-item">
                     <div class="dropdown">
                     <a class="btn btn-secondary" href="./patient_sign_out.php">Logout</a>
                     </div>
                 </li>
-            </ul>
-
+                </ul>
+            </div>
         </nav>
+            
     </div>
+   
